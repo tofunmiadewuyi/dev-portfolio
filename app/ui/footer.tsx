213b2e190c9c87d10da34b76a1ff490e256fb811
Tofunmi Adewuyi <tiadewuyi@gmail.com>
2023-11-25 04:42:05 +0100

    Edited the footer icons

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -7,19 +7,19 @@ export default function Footer() {
             <div className=" flex gap-20 h-[200px]">
                 <Link href='https://github.com/tofunmiadewuyi' target="_blank"
                 className="overflow-hidden rounded-[20px] backdrop-blur-sm hover:shadow-card hover:h-[320px] hover:-translate-y-[130px] hover:bg-white/30 transition duration-150 ease-in-out">
-                    <span className="opacity-60 hover:opacity-100">
+                    <span className="opacity-50 grayscale hover:opacity-100 hover:grayscale-0">
                     <GitHubIcon color="black" size={360} />
                     </span>
                 </Link>
                 <Link href='https://www.linkedin.com/in/tofunmi-adewuyi-173a8012a/' target="_blank"
                 className=" overflow-hidden rounded-[20px] backdrop-blur-sm hover:shadow-card hover:h-[320px] hover:-translate-y-[130px] hover:bg-white/30 transition duration-150 ease-in-out">
-                    <span className="opacity-60 hover:opacity-100">
+                    <span className="opacity-60 grayscale hover:opacity-100 hover:grayscale-0">
                     <LinkedInIcon/>
                    </span>
                 </Link>
                 <Link href='https://dribbble.com/7ofunm1' target="_blank"
                 className=" overflow-hidden rounded-[20px] backdrop-blur-sm hover:shadow-card hover:h-[320px] hover:-translate-y-[130px] hover:bg-white/30 transition duration-150 ease-in-out">
-                    <span className="opacity-60 hover:opacity-100">
+                    <span className="opacity-60 grayscale hover:opacity-100 hover:grayscale-0">
                     <DribbleIcon/>
                    </span>
                 </Link>
