f5e3de5172414e0c03fd23060c012f871ba44a00
Tofunmi Adewuyi <tiadewuyi@gmail.com>
2023-11-25 04:56:28 +0100

    added commmand k focus in a useEffect

--- app/ui/topbar.tsx
+++ app/ui/topbar.tsx
@@ -1,7 +1,29 @@
+'use client'
+
+import { useEffect, useRef } from "react"
 import { LogoIcon, SearchIcon } from "../icons"
 
 
 export default function Topbar() {
+
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    useEffect(() => {
+        const handleKeypress = (event: KeyboardEvent) => {
+            const isCommand = event.metaKey || event.ctrlKey
+
+            if(isCommand && event.key === 'k' && inputRef) {
+                console.log('Cmd k pressed')
+                inputRef.current?.focus()
+            }
+        }
+
+        window.addEventListener('keydown', handleKeypress)
+
+        return () => {
+            window.removeEventListener('keydown', handleKeypress)
+        }
+    })
     return (
         <div className="text-myblack z-50 bg-white w-screen fixed border-b border-myblack/7 shadow-topbar h-16 justify-between p-3 flex items-center">
             <span className="flex gap-1.5 items-center">
@@ -12,6 +34,7 @@ export default function Topbar() {
                 <input 
                     type="text"
                     placeholder="Search"
+                    ref={inputRef}
                     className="h-10 max-w-[264px] placeholder:text-sm placeholder:text-myblack/70 border border-myblack/5 rounded-xl px-[34px] bg-[#F9FAFC]"
                     style={{boxShadow: '0px -1px 4px 0px rgba(22, 22, 22, 0.07) inset'}}/>
                 <span className="absolute flex top-0 h-full px-2.5 my-auto left-0">
