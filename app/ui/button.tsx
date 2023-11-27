ed06d9b210ea1b9d46ae16d82c33b0a62d492b4e
Tofunmi Adewuyi <40152992+tofunmiadewuyi@users.noreply.github.com>
2023-11-27 18:49:22 +0100

    Update button.tsx
    Make the CTAs open new tabs

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 interface ButtonProps {children: React.ReactNode, href: string}
 
 export const BlackButton:React.FC<ButtonProps> = ({children, href}) => {
-    return <Link href={href}
+    return <Link href={href} target="_blank"
     className=" h-10 rounded-[10px] flex gap-1 justify-center items-center px-3 bg-myblack
     text-white text-[15px] leading-5 border border-white/20 shadow-blackbutton font-medium
     hover:scale-102 hover:bg-black transition duration-75 ease-out">
@@ -12,7 +12,7 @@ export const BlackButton:React.FC<ButtonProps> = ({children, href}) => {
 
 export const WhiteButton:React.FC<ButtonProps> = ({children, href}) => {
 
-    return <Link href={href}
+    return <Link href={href} target="_blank"
         className=" h-10 rounded-[10px] flex gap-1 justify-center items-center px-3 bg-white
         text-myblack text-[15px] leading-5 border border-myblack/7 shadow-whitebutton font-medium
         hover:scale-102 hover:bg-background hover:shadow-none transition duration-75 ease-out">
